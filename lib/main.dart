183cd35ea622edc8c43e37f48d62c2d86111f732
Greg Colley <greg.colley@gmail.com>
2022-05-31 08:15:03 +0100

    Dice app example
    https://www.linkedin.com/learning/flutter-part-04-building-an-app-with-state?u=67829530

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,9 +16,56 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  //const DicePage({Key? key}) : super(key: key);
+
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void changeDiceFace() {
+    leftDiceNumber = Random().nextInt(6) + 1;
+    rightDiceNumber = Random().nextInt(6) + 1;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    changeDiceFace();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: TextButton(
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+              onPressed: () {
+                setState(() {
+                  changeDiceFace();
+                });
+
+                // Like a console.log (Results in the run console)
+                print('Left button got pressed [$leftDiceNumber]');
+              },
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              child: Image.asset('images/dice$rightDiceNumber.png'),
+              onPressed: () {
+                setState(() {
+                  changeDiceFace();
+                });
+
+                // Like a console.log (Results in the run console)
+                print('Right button got pressed [$rightDiceNumber]');
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
